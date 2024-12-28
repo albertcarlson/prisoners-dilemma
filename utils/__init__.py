f0ac24399f056750bf466d80e661b71f4094a338
Albert Carlson <albertcarlson@Alberts-MacBook-Air.local>
2024-12-28 23:07:45 +0100

    Rename dtypes.py to simulation_utils.py to reflect updates

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,12 @@
 # Import in __init__.py to make it available in the package
-from utils.dtypes import (
-    Strategy, Action, History, Player, battle, round_probabilistically, random_action, PAYOFF_MATRIX, STARTING_POPULATION
+from utils.simulation_utils import (
+    Strategy, 
+    Action, 
+    History, 
+    Player,
+    battle, 
+    round_probabilistically, 
+    random_action, 
+    PAYOFF_MATRIX, 
+    STARTING_POPULATION
 )
