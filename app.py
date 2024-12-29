0f38539e96db1f5d983b430eae45a8aa1d8767f1
Albert Carlson <albertcarlson@Alberts-MacBook-Air.local>
2024-12-29 17:53:35 +0100

    MVP for the streamlit simulation

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from utils import PAYOFF_MATRIX, STARTING_POPULATION, Action
+from utils import PAYOFF_MATRIX, STARTING_POPULATION, Action, Player, Population, Strategy
 from catalogue import EXAMPLE_SPECIES
-from stqdm import stqdm
+from collections.abc import Iterable
+#from stqdm import stqdm
 import streamlit as st
 import pandas as pd
-import time
+#import time
+
+
+
 
 st.title("Prisoner's Dilemma Ecological Simulation")
 
@@ -25,24 +29,22 @@ df = pd.DataFrame({
     "Opponent DEFECTS": [PAYOFF_MATRIX[(Action.COOP, Action.DEFECT)][0], PAYOFF_MATRIX[(Action.DEFECT, Action.DEFECT)][0]],
 }).set_index("Your Reward")
 st.table(df)
-st.write("*At the moment, you need to change `config.ini` to adjust with the payoff matrix.*")
+st.info("*At the moment, you need to change `config.ini` to adjust with the payoff matrix.*")
 
 
 
 st.subheader("Simulation parameters")
-col1, col2, col3, col4 = st.columns(4)
+advanced = st.checkbox("Advanced")
+col1, col2, col3 = st.columns(3)
 starting_population = col1.number_input("Starting population per species", value=STARTING_POPULATION, min_value=1, max_value=100, step=1, disabled=True)
 
-rounds = col2.number_input("Rounds per battle", value=50, min_value=1, max_value=1000, step=5)
-st.write("The scoring is normalized, i.e. is an average for the rounds. Therefore, 100 and 500 rounds are probably the same.")
+rounds = col2.number_input("Rounds per battle", value=50, min_value=1, max_value=1000, step=5, disabled=not advanced)
+overall_food = col3.number_input("Overall food available", value=100, min_value=0, max_value=2_000, step=25)
+matchup_rate = col2.number_input("Matchup rate", value=1.0, min_value=0.0, max_value=1.0, step=0.01, disabled=not advanced)
+mutation_rate = col3.number_input("Mutation rate", value=0.01, min_value=0.0, max_value=1.0, step=0.01)
 
-overall_food = col3.number_input("Overall food available", value=1_000, min_value=0, max_value=10_000, step=100)
-
-matchup_rate = col4.number_input("Matchup rate", value=1.0, min_value=0.0, max_value=1.0, step=0.01)
 st.write("Adjust overall food mid-game to introduce famines or periods of plenty.")
-
-st.write("*At the moment, you need to change `config.ini` to adjust the starting population per species.*")
-
+st.write("Adjust mutation rate to increase the probability that offspring uses a different strategy than its parent.")
 
 st.write("\n")
 st.write("***")
@@ -50,15 +52,69 @@ st.write("\n")
 
 
 
-col1, col2 = st.columns(2)
-generation = st.session_state.get("generation", 0)
-col1.button("Run 1 generation")
-col2.button("Run 5 generations")
-st.warning("The simulation is not implemented yet.")
-st.write(f"Generation: {generation}")
-# st.write(f"Total population: {sum(counts.values())}")
+def generate_new_population(
+        starting_population: int = starting_population, 
+        species: Iterable[Strategy] = EXAMPLE_SPECIES.values()
+) -> Population:
+    players = []
+    for strategy in species[:1]:
+        for _ in range(starting_population):
+            players.append(Player(strategy))
+    return Population([players])
 
 
+if len(species) == 0:
+    st.info("To begin the simulation, select at least one species.")
+    st.stop()
+
+
+if "generation" not in st.session_state:
+    st.session_state.generation = 0
+
+if st.session_state.generation == 0:
+    st.session_state.population = generate_new_population(species=[EXAMPLE_SPECIES[strategy] for strategy in species])
+
+
+col1, col2, col3 = st.columns(3)
+
+
+if col1.button("Run 1 generation"):
+    st.session_state.generation += 1
+    st.session_state.population.do_generation(
+        matchup_rate=matchup_rate, 
+        mutation_probability=mutation_rate, 
+        mutation_strategies=[EXAMPLE_SPECIES[strategy] for strategy in species],
+        rounds=rounds, 
+        overall_food=overall_food
+    )
+
+if col2.button("Run 5 generations"):
+    st.session_state.generation += 5
+    for _ in range(5):
+        st.session_state.population.do_generation(
+            matchup_rate=matchup_rate, 
+            mutation_probability=mutation_rate, 
+            mutation_strategies=[EXAMPLE_SPECIES[strategy] for strategy in species],
+            rounds=rounds, 
+            overall_food=overall_food
+        )
+
+if col3.button("Reset simulation"):
+    st.session_state.generation = 0
+    st.session_state.population = generate_new_population(species=[EXAMPLE_SPECIES[strategy] for strategy in species])
+
+st.write(f"Generation: {st.session_state.generation}")
+st.write(f"Total population: {st.session_state.population.population_size}")
+st.write(f"Population average age: {st.session_state.population.population_average_age}")
+
+st.write(f"Mutation probability: {mutation_rate}, Matchup rate: {matchup_rate}, Rounds per battle: {rounds}, Overall food: {overall_food}")
 
 st.markdown("<h4 style='text-align: center;'>Current population counts</h4>", unsafe_allow_html=True)
-st.bar_chart([5, 3, 7, 2, 6], x_label="Species", y_label="Count")
+st.bar_chart(st.session_state.population.get_population_counts(), x_label="Species", y_label="Count")
+
+
+# Stacked area chart of population counts
+if st.session_state.generation > 0:
+    st.markdown("<h4 style='text-align: center;'>Development of population</h4>", unsafe_allow_html=True)
+    st.area_chart([st.session_state.population.get_population_counts(gen) for gen in range(1, st.session_state.generation)], x_label="Generation", y_label="Count")
+
